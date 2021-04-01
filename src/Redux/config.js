3bca5fd65ccdea80ace22752b76f7c76453dbdcb
Alexander Komarov <san4os2008@gmail.com>
2021-04-01 15:06:10 +0300

    Localization. Stage 2.

--- src/Redux/config.js
+++ src/Redux/config.js
@@ -17,6 +17,7 @@
 
 // console.log('supportedLangs3: ', supportedLangs);
 
+console.log('[file]');
 export default {
     // apiUrl:"https://next.json-generator.com/api/json/get/V1TbDaNiK"
     // apiUrl:"https://jsonplaceholder.typicode.com/"
