3fc5f3f0b01ca797eb5666117afd8f90eec5adb1
katenew <kkrat@ARKTUR>
2021-04-14 20:17:50 +0300

    Add exhisting file

--- src/Components/Pages/ThirdPageofSearchModule/BookButton.js
+++ src/Components/Pages/ThirdPageofSearchModule/BookButton.js
@@ -7,7 +7,7 @@ import {CartDemo} from '../Cart/Cart'
 export const BookButton = ({hotel_room_id,value,totalPax,date,tour_id,hotelChoice,tour_room,hotel_id,tariff}) =>{
     
 
-    console.log("TARIFF",tariff[0])
+    console.log("TARIFF",tariff[0],tour_room.id)
     const [booking,setBooking] = useState({})
 
      const AddToBasket=()=>{
@@ -31,12 +31,14 @@ export const BookButton = ({hotel_room_id,value,totalPax,date,tour_id,hotelChoic
             <>
             {
                 tariff&&tariff[0].map((item)=>{
+                 if(item.smart_category_id === tour_room.id){
                    return item.rates.map((item1)=>{
-                    if(item1.tariff_name.match(/\=.+/)[0].replace('=','') === hotel_id&&item.smart_category_id === tour_room.id)
+                    // if(item1.tariff_name.match(/\=.+/)[0].replace('=','') === hotel_id&&item.smart_category_id === tour_room.id)
                     //   return item1.rate_details.filter((item2,index,array)=>array.findIndex(t=>t.smart_category_id===item2.smart_category_id)===index).map((item3)=>{
+                      
                         return item1.rate_details.map((item3)=>{
-                        if(totalPax.counterAdults >= item3.min_adult&&totalPax.counterAdults<=item3.max_adult)
-                         return(
+                          if(totalPax.counterAdults >= item3.min_adult&&totalPax.counterAdults<=item3.max_adult){
+                           return(
                             <div class='WrapperAddToBasket'>
                                 <h4>{
                                 // item1.tariff_name.match(/\=.+/)[0].replace('=','')+
@@ -44,9 +46,11 @@ export const BookButton = ({hotel_room_id,value,totalPax,date,tour_id,hotelChoic
                                 <button class='AddToBasketButton' onClick={AddToBasket}>Add to Basket</button>
                             </div>
                         )
-                  
+                       }
+                     
                     })
                 })
+              }
             })
             }
             </>
