026aa28246c798b48c5f37ad18c4f11a65b666bd
Alexander Komarov <san4os2008@gmail.com>
2021-04-27 21:27:46 +0300

    Some little fixings.

--- src/Components/Pages/BookingForm/CartDetails.js
+++ src/Components/Pages/BookingForm/CartDetails.js
@@ -7,49 +7,127 @@ export const CartDetails = ({cart}) =>{
 
     const [sendCart, setSendCart] = useState([]);
 
+    // const sc = axios.post('http://smartbooker.biz/interface/xmlsubj/', JSON.stringify({"title": {
+    //         "username":"Serodynringa",
+    //         "password":"%tmMJZbABm6cB@tY",
+    //         "user_id" :1426,
+    //         "action":"AddToCartRQ",
+    //
+    //         "data" :
+    //             {
+    //                 "bookings":
+    //                     [
+    //                         {
+    //                             "start" : cart.start,
+    //                             "tour_id" : cart.tour_id,
+    //                             "tour_tariff_id" : cart.tour_tariff_id,
+    //                             "tour_room_id" : cart.tour_room_id,
+    //                             "numberofunits" : 1,
+    //                             "hotel_id" : cart.hotel_id,
+    //                             "hotel_room_id" : cart.hotel_room_id,
+    //                             "hotel_rate_id" : null,
+    //                             "calculation_data" :
+    //                                 {
+    //                                     "adults":cart.adults,
+    //                                     "children":cart.children,
+    //                                     "amount" : null
+    //                                 }
+    //                         }
+    //                     ]
+    //             }
+    //     }}))
+    //     .then(response => console.log('SENDCART-2',response.data));
+        // .then(response => setSendCart(response.data));
+
     useEffect(() => {
-        const AddToCartRQ = { 
-          title: {
-            // "username":"Serodynringa",
-            // "password":"%tmMJZbABm6cB@tY",
-            // "user_id" :1426, 
-            // "action":"AddToCartRQ",
-                
-                  "data" : 
+        const AddToCartRQ = {
+            "title": {
+                "username":"Serodynringa",
+                "password":"%tmMJZbABm6cB@tY",
+                "user_id" :1426,
+                "action":"AddToCartRQ",
+
+                "data" :
                     {
-                       "bookings":
-                          [
-                            {
-                              "start" : cart.start, 
-                              "tour_id" : cart.tour_id,      
-                              "tour_tariff_id" : cart.tour_tariff_id,   
-                              "tour_room_id" : cart.tour_room_id, 
-                              "numberofunits" : 1,    
-                              "hotel_id" : cart.hotel_id,
-                              "hotel_room_id" : cart.hotel_room_id,
-                              "hotel_rate_id" : null,  
-                              "calculation_data" : 
-                                             {   
-                                              "adults":cart.adults,
-                                              "children":cart.children,    
-                                              "amount" : null
-                                            }
+                        "bookings":
+                            [
+                                {
+                                    "start" : cart.start,
+                                    "tour_id" : cart.tour_id,
+                                    "tour_tariff_id" : cart.tour_tariff_id,
+                                    "tour_room_id" : cart.tour_room_id,
+                                    "numberofunits" : 1,
+                                    "hotel_id" : cart.hotel_id,
+                                    "hotel_room_id" : cart.hotel_room_id,
+                                    "hotel_rate_id" : null,
+                                    "calculation_data" :
+                                        {
+                                            "adults":cart.adults,
+                                            "children":cart.children,
+                                            "amount" : cart.amount
+                                        }
                                 }
-                          ]
-                        }
-                    } 
-                };
+                            ]
+                    }
+            }
+        };
 
-        const headers = {"username":"Serodynringa",
-        "password":"%tmMJZbABm6cB@tY",
-        "user_id" :1426, 
-        "action":"AddToCartRQ"}
+        // const headers = {"username":"Serodynringa",
+        //     "password":"%tmMJZbABm6cB@tY",
+        //     "user_id" :1426,
+        //     "action":"AddToCartRQ"}
 
-        axios.post('http://smartbooker.biz/interface/xmlsubj/', AddToCartRQ,{headers})
-        .then(response => setSendCart(response.data));
+        axios.post('http://smartbooker.biz/interface/xmlsubj/', JSON.stringify({AddToCartRQ}))
+            .then(response => setSendCart(response.data));
+
+        // axios.post('http://smartbooker.biz/interface/xmlsubj/', AddToCartRQ,{headers})
+        //     .then(response => setSendCart(response.data));
 
     }, []);
-    
+
+    // useEffect(() => {
+    //     const AddToCartRQ = {
+    //         title: {
+    //             // "username":"Serodynringa",
+    //             // "password":"%tmMJZbABm6cB@tY",
+    //             // "user_id" :1426,
+    //             // "action":"AddToCartRQ",
+    //
+    //             "data" :
+    //                 {
+    //                     "bookings":
+    //                         [
+    //                             {
+    //                                 "start" : cart.start,
+    //                                 "tour_id" : cart.tour_id,
+    //                                 "tour_tariff_id" : cart.tour_tariff_id,
+    //                                 "tour_room_id" : cart.tour_room_id,
+    //                                 "numberofunits" : 1,
+    //                                 "hotel_id" : cart.hotel_id,
+    //                                 "hotel_room_id" : cart.hotel_room_id,
+    //                                 "hotel_rate_id" : null,
+    //                                 "calculation_data" :
+    //                                     {
+    //                                         "adults":cart.adults,
+    //                                         "children":cart.children,
+    //                                         "amount" : null
+    //                                     }
+    //                             }
+    //                         ]
+    //                 }
+    //         }
+    //     };
+    //
+    //     const headers = {"username":"Serodynringa",
+    //         "password":"%tmMJZbABm6cB@tY",
+    //         "user_id" :1426,
+    //         "action":"AddToCartRQ"}
+    //
+    //     axios.post('http://smartbooker.biz/interface/xmlsubj/', AddToCartRQ,{headers})
+    //         .then(response => setSendCart(response.data));
+    //
+    // }, []);
+
     console.log('SENDCART',sendCart)
     return(
         <h2 class='CartDetails'>
