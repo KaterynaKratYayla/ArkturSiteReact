de7fddbd6b8610d26926f8af144f0321036d2ebe
Alexander Komarov <san4os2008@gmail.com>
2021-05-27 16:19:39 +0300

    Added LiqPay

--- src/Components/Pages/BookingForm/ConfirmButton.js
+++ src/Components/Pages/BookingForm/ConfirmButton.js
@@ -3,18 +3,19 @@ import axios from 'axios'
 
 import './ConfirmButton.css'
 import Item from 'antd/lib/list/Item';
+import {Example} from '../../Library/LiqPay/Example';
 
 export const ConfirmButton = ({AddContacts, clientData, booking,clicked}) =>{
 
     console.log('CLIENTDATA1', clientData[0].name)
     // console.log('CLIENTDATA2',Object.entries(booking[0]))
-    
+
     const [ModifyClientsRQ_Add, setModifyClientsRQ_Add] = useState([]);
     // const [visible,setVisible] = useState(false)
     // const [clicked,setClicked] = useState(false)
-  
+
         let service_id_booking;
-        
+
         for (let key in booking[0]){
             if(key === 'data'){
                 for (let i in booking[0][key]){
@@ -35,34 +36,34 @@ export const ConfirmButton = ({AddContacts, clientData, booking,clicked}) =>{
         const ActionRQ = {
                 "username":"Serodynringa",
                 "password":"%tmMJZbABm6cB@tY",
-                "user_id" :1426, 
+                "user_id" :1426,
                 "action":"ModifyClientsRQ",
-                "data" : 
+                "data" :
                     {
                         "service_id" : service_id_booking,
                         "service_number" : 1,
-                        "clients" : 
-                            [	
+                        "clients" :
+                            [
                                 {
-                                    "operation" : "add", 
-                                    "client_type" : "buyer", 
+                                    "operation" : "add",
+                                    "client_type" : "buyer",
                                     "site_client_id" : 1,
                                     // "name" : clientData[0].name,
                                     // "surname" : clientData[0].surname,
-                                    // "phone" : clientData[0].phone, 
-                                    // "email" : clientData[0].email 
+                                    // "phone" : clientData[0].phone,
+                                    // "email" : clientData[0].email
                                     "name" : "Kate",
                                     "surname" : "Krat",
-                                    "phone" : "+380933266802", 
+                                    "phone" : "+380933266802",
                                     "email" : "kkrat@arktur.ua"
                                 },
                                 {
-                                    "operation" : "add", 
-                                    "client_type" : "client", 
+                                    "operation" : "add",
+                                    "client_type" : "client",
                                     "site_client_id" : 2,
                                     "name" : "Gogel",
                                     "surname" : "Mogel",
-                                    "phone" : "+38 (044) 490 7114", 
+                                    "phone" : "+38 (044) 490 7114",
                                     "email" : "atest@arktur.ua"
                                 },
                            ]
@@ -72,14 +73,14 @@ export const ConfirmButton = ({AddContacts, clientData, booking,clicked}) =>{
         axios.post('http://smartbooker.biz/interface/xmlsubj/', JSON.stringify({ActionRQ}))
             .then(response => setModifyClientsRQ_Add(response.data));
     }, []);
-  
+
     console.log('ModifyClientsRQ_Add',ModifyClientsRQ_Add)
 
     return(
       <>
       {/* <div>{service_id_booking}</div> */}
-      <button 
-            type='submit' 
+      <button
+            type='submit'
             class={clicked === false? 'SubmitButton':'NonActiveSubmit'}
             onClick={AddContacts}>
                 Confirm Details
@@ -92,7 +93,12 @@ export const ConfirmButton = ({AddContacts, clientData, booking,clicked}) =>{
             </button>
             ):null
         }
-      </>  
+       {
+         clicked === true?(
+             <Example />
+            ):null
+        }
+      </>
     )
 }
 
