710c524ce330524c57ecd1658db105b57f141738
Alexander Komarov <san4os2008@gmail.com>
2021-06-09 10:47:50 +0300

    First request to Smart after payment.

--- src/Components/Pages/LiqPay/PaymentResult.js
+++ src/Components/Pages/LiqPay/PaymentResult.js
@@ -1,7 +1,36 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import axios from "axios";
 
 const PaymentResult = () => {
-    return <div>Congrats!</div>;
+
+    const [paymentInfo, setPaymentInfo] = useState(null);
+
+    useEffect(() => {
+        const ActionRQ = {
+            "username":"Serodynringa",
+            "password":"%tmMJZbABm6cB@tY",
+            "user_id" :1426,
+            "action":"GetPaymentInfo",
+            "data" :
+                {
+                    "service_id":3
+                }
+        };
+
+        axios.post('http://smartbooker.biz/interface/xmlsubj/', JSON.stringify({ActionRQ}))
+            .then(response => setPaymentInfo(response.data))
+            .catch(error =>{
+                setPaymentInfo(undefined)
+                console.log('[axios error]: ', error)
+            });
+
+    }, []);
+    return (
+        <div>
+            <p>Congrats!</p>
+            <p>{paymentInfo}</p>
+        </div>
+    );
 }
 
 export default PaymentResult;
