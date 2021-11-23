f59e8c6909ea273f576bc9979c2b7f9127139fdc
Alexander Komarov <san4os2008@gmail.com>
2021-11-23 12:41:52 +0200

    Added SSR Possibility

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom';
+import { hydrate, render } from "react-dom";
 import { Provider } from "react-redux";
 // import './index.css';
 import App from './App';
@@ -7,14 +8,35 @@ import reportWebVitals from './reportWebVitals';
 import store from "./Redux/redux/store";
 
 
-ReactDOM.render(
+/* ReactDOM.render(
   <React.StrictMode>
       <Provider store={store}>
           <App />
         </Provider>
   </React.StrictMode>,
   document.getElementById('root')
-);
+); */
+
+const rootElement = document.getElementById("root");
+if (rootElement.hasChildNodes()) {
+  hydrate(
+    <React.StrictMode>
+        <Provider store={store}>
+            <App />
+          </Provider>
+    </React.StrictMode>,
+    rootElement
+    );
+} else {
+  render(
+    <React.StrictMode>
+        <Provider store={store}>
+            <App />
+          </Provider>
+    </React.StrictMode>,
+    rootElement
+    );
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
