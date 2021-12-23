1b15e3677d38b61a0dc3963ce181c3fe960849ec
Alexander Komarov <san4os2008@gmail.com>
2021-12-23 15:24:37 +0200

    Added language to GetPaymentInfoRQ

--- src/Components/Library/LiqPay/Pay.js
+++ src/Components/Library/LiqPay/Pay.js
@@ -2,6 +2,8 @@ import React from "react";
 import {useIntl} from 'react-intl'
 import { LiqPayPay } from "react-liqpay";
 import {useSelector} from "react-redux";
+import {useLocation} from "react-router-dom"
+import {ValidateQuery} from '../../Pages/Helpers/helper'
 
 console.log('[file]', 'src/Components/Library/LiqPay/Example.js');
 
@@ -14,15 +16,19 @@ export const Pay = ({service_id, smart_order_id,cart,client, service_number}) =>
     const {locale} = useIntl();
     console.log('locale: ', locale)
 	const { user: currentUser } = useSelector((state) => state.auth);
+    const location = useLocation();
+    let search_data = ValidateQuery(location);
 
     const CryptoJS = require("crypto-js");
 
-    // TODO: Взять откуда-то site_order_id, site_service_id и smart_service_number
+    // TODO: Взять откуда-то site_order_id и site_service_id
     const orderData = {
         "username":"Serodynringa",
         "password":"%tmMJZbABm6cB@tY",
         // "user_id" :1426,
         "user_id" :currentUser.user_id,
+        "refpartner":search_data.refpartner?search_data.refpartner:null,
+        "language":locale,
         "action":"GetPaymentInfoRQ",
         "data" :
             {
@@ -33,6 +39,13 @@ export const Pay = ({service_id, smart_order_id,cart,client, service_number}) =>
                 "smart_service_number": service_number,		// На будущее, когда в заказе будет разрешено несколько сервисов - booking_entity.ref_serv_smart
             }
     }
+
+    // Хотел зашиврофать state и восстановить его позже в Redux
+	/* const { state } = useSelector((state) => state);
+    // console.log('state: ', state);
+
+    const cipherstate = CryptoJS.AES.encrypt(JSON.stringify(state), process.env.REACT_APP_PRIVATE_KEY).toString();
+    localStorage.setItem('state', cipherstate); */
 
     const ciphertext = CryptoJS.AES.encrypt(JSON.stringify(orderData), process.env.REACT_APP_PRIVATE_KEY).toString();
     localStorage.setItem('orderData', ciphertext);
